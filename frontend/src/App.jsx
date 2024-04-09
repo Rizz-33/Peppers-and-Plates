b1faf400c76e8712dcbdb1d4f1627894e9ea2e0b
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-04-09 20:50:32 +0530

    Add navabr component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from './components/navabr/Navbar'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='app'>
+      <Navbar/>
+    </div>
   )
 }
 
