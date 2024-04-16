c4355ed83e4a3e8c23beeba2526f0a49aa61d339
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-04-17 00:22:17 +0530

    feat: Create routes for home, cart and place orders

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 import Navbar from './components/navabr/Navbar'
+import Cart from './pages/cart/Cart'
+import Home from './pages/home/Home'
+import Order from './pages/placeOrder/Order'
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/order' element={<Order/>}/>
+      </Routes>
     </div>
   )
 }
---
+++ frontend/src/pages/placeOrder/Order.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Order.css'
+
+const Order = () => {
+    return (
+        <div>Order</div>
+    )
+}
+
+export default Order
