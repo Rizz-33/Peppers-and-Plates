17ffeea2a2f58f040a1a0391c7bac9e42915194b
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-04-17 21:14:29 +0530

    refactor: ExploreMenu

--- frontend/src/components/exploreMenu/ExploreMenu.jsx
+++ frontend/src/components/exploreMenu/ExploreMenu.jsx
@@ -11,8 +11,8 @@ const ExploreMenu = ({ category, setCategory }) => {
                 {menu_list.map((item, index) => {
                     return (
                         <div onClick={() => setCategory(prev => prev === item.menu_name ? "All" : item.menu_name)} key={index} className="explore-menu-list-item">
-                            <img src={item.menu_image} className={category === item.menu_name ? "active" : ""} alt="" />
-                            <p>{item.menu_name}</p>
+                            <img src={item.menu_image} className={category === item.menu_name ? "active-image" : ""} alt="" />
+                            <p className={category === item.menu_name ? "active-name" : ""}>{item.menu_name}</p>
                         </div>
                     )
                 })}
