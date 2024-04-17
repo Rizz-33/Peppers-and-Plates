78ebaece1aff438221c9d31d2a8c90c7157ae533
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-04-17 20:48:41 +0530

    feat: Menu list

---
+++ frontend/src/components/exploreMenu/ExploreMenu.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { menu_list } from '../../assets/assets';
+import './ExploreMenu.css';
+
+const ExploreMenu = ({ category, setCategory }) => {
+    return (
+        <div className='explore-menu' id='explore-menu'>
+            <h1>Embark on Flavorful Adventures</h1>
+            <p className='explore-menu-text'>Explore our exquisite menu, indulge in flavorful adventures. From appetizers to desserts, find satisfaction for every palate. Join us for culinary delight!</p>
+            <div className="explore-menu-list">
+                {menu_list.map((item, index) => {
+                    return (
+                        <div onClick={() => setCategory(prev => prev === item.menu_name ? "All" : item.menu_name)} key={index} className="explore-menu-list-item">
+                            <img src={item.menu_image} className={category === item.menu_name ? "active" : ""} alt="" />
+                            <p>{item.menu_name}</p>
+                        </div>
+                    )
+                })}
+            </div>
+            <hr />
+        </div>
+    )
+}
+
+export default ExploreMenu;
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import Header from '../../components/header/Header'
-import './Home.css'
+import React, { useState } from 'react';
+import ExploreMenu from '../../components/exploreMenu/ExploreMenu';
+import Header from '../../components/header/Header';
+import './Home.css';
 
 const Home = () => {
+    const [category, setCategory] = useState("All");
+
     return (
         <div>
-            <Header/>
+            <Header />
+            <ExploreMenu category={category} setCategory={setCategory} />
         </div>
-    )
+    );
 }
 
-export default Home
+export default Home;
