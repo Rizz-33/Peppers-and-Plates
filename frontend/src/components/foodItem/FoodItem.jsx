a3b80f580580a1fe2905493a65707b3b1f01ee15
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-04-25 20:37:33 +0530

    feat: Add button

--- frontend/src/components/foodItem/FoodItem.jsx
+++ frontend/src/components/foodItem/FoodItem.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { assets } from '../../assets/assets'
 import './FoodItem.css'
 
 const FoodItem = ({id,name,price,description,image}) => {
+
+    const [itemCount,setItemCount] = useState(0)
+
     return (
         <div className='food-item'>
             <div className="food-item-img-container">
                 <img className='food-item-image' src={image} alt="" />
+                {!itemCount
+                    ?<img className='add' onClick={()=>setItemCount(prev=>perv+1)} src={assets.add_icon_green} alt=''/>
+                    :<div className='food-item-counter'>
+
+                    </div>
+                }
             </div>
             <div className="food-item-info">
                 <div className="food-item-name-rating">
