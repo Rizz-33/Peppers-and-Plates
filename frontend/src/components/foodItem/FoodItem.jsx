778b11d4c3f11219b3febc8440497030c34b45fd
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-04-27 20:48:53 +0530

    fix: Functionality with the add button

--- frontend/src/components/foodItem/FoodItem.jsx
+++ frontend/src/components/foodItem/FoodItem.jsx
@@ -10,14 +10,14 @@ const FoodItem = ({id,name,price,description,image}) => {
         <div className='food-item'>
             <div className="food-item-img-container">
                 <img className='food-item-image' src={image} alt="" />
-                {!itemCount
-                    ?<img className='add' onClick={()=>setItemCount(prev=>perv+1)} src={assets.add_icon_green} alt=''/>
+            </div>
+            <div className="food-item-info">
+                <div style={{ textAlign: 'right' }}>{!itemCount
+                    ?<img className='add' onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_green} alt=''/>
                     :<div className='food-item-counter'>
 
                     </div>
-                }
-            </div>
-            <div className="food-item-info">
+                }</div>
                 <div className="food-item-name-rating">
                     <p>{name}</p>
                     <img src={assets.rating_starts} alt="" />
