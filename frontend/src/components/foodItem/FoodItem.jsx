c54f8f71620a504c3acfdfd277b5d9204d68ae52
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-04-27 21:33:56 +0530

    refactor: Add or remove one

--- frontend/src/components/foodItem/FoodItem.jsx
+++ frontend/src/components/foodItem/FoodItem.jsx
@@ -10,16 +10,16 @@ const FoodItem = ({id,name,price,description,image}) => {
         <div className='food-item'>
             <div className="food-item-img-container">
                 <img className='food-item-image' src={image} alt="" />
-            </div>
-            <div className="food-item-info">
-                <div style={{ textAlign: 'right' }}>{!itemCount
-                    ?<img className='add-remove' onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_green} alt=''/>
+                {!itemCount
+                    ?<img className='add' onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_green} alt=''/>
                     :<div className='food-item-counter'>
                         <img className='add-remove' onClick={()=>setItemCount(prev=>prev-1)} src={assets.remove_icon_red} alt=''/>
                         <p>{itemCount}</p>
                         <img className='add-remove' onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_green} alt="" />
                     </div>
-                }</div>
+                }
+            </div>
+            <div className="food-item-info">
                 <div className="food-item-name-rating">
                     <p>{name}</p>
                     <img src={assets.rating_starts} alt="" />
