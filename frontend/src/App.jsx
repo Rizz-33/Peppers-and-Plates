d65568308aa9278da132a063ef303dcf820fd364
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-04-28 19:08:53 +0530

    feat: Footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import Footer from './components/footer/Footer'
 import Navbar from './components/navabr/Navbar'
 import Cart from './pages/cart/Cart'
 import Home from './pages/home/Home'
@@ -7,14 +8,17 @@ import Order from './pages/placeOrder/Order'
 
 const App = () => {
   return (
-    <div className='app'>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/cart' element={<Cart/>}/>
-        <Route path='/order' element={<Order/>}/>
-      </Routes>
-    </div>
+    <>
+      <div className='app'>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/order' element={<Order/>}/>
+        </Routes>
+      </div>
+      <Footer />
+    </>
   )
 }
 
---
+++ frontend/src/components/footer/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { assets } from '../../assets/assets'
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div className='footer' id='footer'>
+            <div className="footer-content">
+                <div className="footer-content-left">
+                    <img src={assets.logo} alt="" className='logo'/>
+                    <p></p>
+                    <div className="footer-social-icons">
+                        <img src={assets.facebook_icon} alt="" className='facebook'/>
+                        <img src={assets.twitter_icon} alt="" className='twitter'/>
+                        <img src={assets.linkedin_icon} alt="" className='likedin'/>
+                    </div>
+                </div>
+                <div className="footer-content-center">
+                    <h2>Company</h2>
+                    <ul>
+                        <li>Home</li>
+                        <li>About Us</li>
+                        <li>Delivery</li>
+                        <li>Privacy Policy</li>
+                    </ul>
+                </div>
+                <div className="footer-content-right">
+                    <h2>Get In Touch</h2>
+                    <ul>
+                        <li>+94 70 124 4832</li>
+                        <li>contact@pappersandplates.com</li>
+                    </ul>
+                </div>
+            </div>
+            <hr />
+            <p className="footer-copyright">Copyright 2024 © papperandplates.com - All Right Reserved.</p>
+        </div>
+    )
+}
+
+export default Footer
