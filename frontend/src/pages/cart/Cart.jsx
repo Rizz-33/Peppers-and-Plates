28423e235c144f7e8ed59d9208bdbc432b07fd61
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-04-30 12:09:44 +0530

    feat: Cart

--- frontend/src/pages/cart/Cart.jsx
+++ frontend/src/pages/cart/Cart.jsx
@@ -19,6 +19,55 @@ const Cart = () => {
                 </div>
                 <br />
                 <hr />
+                {food_list.map((item,index)=>{
+                    if(cartItems[item._id]>0)
+                    {
+                        return(
+                            <div>
+                                <div className="cart-items-title cart-items-item">
+                                    <img src={item.image} alt="" />
+                                    <p>{item.name}</p>
+                                    <p>{item.price} LKR</p>
+                                    <p>{cartItems[item._id]}</p>
+                                    <p>{item.price*cartItems[item._id]} LKR</p>
+                                    <p onClick={()=>removeFromCart(item._id)} className='cross'>x</p>
+                                </div>
+                                <hr />
+                            </div>
+                        )
+                    }
+                })}
+            </div>
+            <div className="cart-bottom">
+                <div className="cart-total">
+                    <h2>Your Order</h2>
+                    <div>
+                        <div className="cart-total-details">
+                            <p>Subtotal</p>
+                            <p>{0}</p>
+                        </div>
+                        <hr />
+                        <div className="cart-total-details">
+                            <p>Delivery Fee</p>
+                            <p>{2}</p>
+                        </div>
+                        <hr />
+                        <div className="cart-total-details">
+                            <p>Total</p>
+                            <p>{0}</p>
+                        </div>
+                    </div>
+                    <button>PROCEED TO CHECKOUT</button>
+                </div>
+                <div className="cart-promocode">
+                    <div>
+                        <p>If you have a promo code, Enter it here</p>
+                        <div className="cart-promocode-input">
+                            <input type="text" placeholder='  #23fhjb38'/>
+                            <button>Submit</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
