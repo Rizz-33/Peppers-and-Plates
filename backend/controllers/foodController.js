030c88edee6a358adc2c1c66343455eba004895c
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-04-30 16:15:10 +0530

    feat: Remove images

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import foodModel from '../models/foodModel.js ';
 
 //add food item
@@ -32,4 +33,18 @@ const listFood = async (req,res) => {
     }
 }
 
-export { addFood, listFood };
+//remove food item
+const removeFood = async (req,res) => {
+    try {
+        const food = await foodModel.findById(req.body.id);
+        fs.unlink(`uploads/${food.image}`,()=>{})
+
+        await foodModel.findByIdAndDelete(req.body.id);
+        res.json({success:true,message:"Food Removed"})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
+}
+
+export { addFood, listFood, removeFood };
--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 import multer from "multer";
-import { addFood, listFood } from "../controllers/foodController.js";
+import { addFood, listFood, removeFood } from "../controllers/foodController.js";
 
 const foodRouter = express.Router();
 
@@ -16,6 +16,7 @@ const upload = multer({storage:storage})
 
 foodRouter.post("/add",upload.single("image"),addFood)
 foodRouter.get("/list",listFood)
+foodRouter.post("/remove",removeFood)
 
 
 export default foodRouter;
