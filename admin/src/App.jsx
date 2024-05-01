7390142b9ad6e6a1d3598b1578df33107c3b5757
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-05-01 17:51:26 +0530

    feat: Add pages and refactoring some files

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 import Navbar from './components/navbar/Navbar'
 import Sidebar from './components/sidebar/Sidebar'
+import Add from './pages/add/Add'
+import List from './pages/list/List'
+import Orders from './pages/orders/Orders'
 
 const App = () => {
   return (
@@ -9,6 +13,11 @@ const App = () => {
       <hr />
       <div className="app-content">
         <Sidebar />
+        <Routes>
+          <Route path='/add' element={<Add/>}/>
+          <Route path='/list' element={<List/>}/>
+          <Route path='/orders' element={<Orders/>}/>
+        </Routes>
       </div>
     </div>
   )
--- admin/src/components/sidebar/Sidebar.jsx
+++ admin/src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import { assets } from '../../assets/assets'
 import './Sidebar.css'
 
@@ -6,18 +7,18 @@ const Sidebar = () => {
     return (
         <div className='sidebar'>
             <div className="sidebar-options">
-                <div className="side-option">
+                <NavLink to='/add' className="side-option">
                     <img src={assets.add_icon} alt="" />
                     <p>Add Items</p>
-                </div>
-                <div className="side-option">
+                </NavLink>
+                <NavLink to='/list' className="side-option">
                     <img className='list-icon' src={assets.list_icon} alt="" />
                     <p>List Items</p>
-                </div>
-                <div className="side-option">
+                </NavLink>
+                <NavLink to='/orders' className="side-option">
                     <img src={assets.order_icon} alt="" />
                     <p>Orders</p>
-                </div>
+                </NavLink>
             </div>
         </div>
     )
