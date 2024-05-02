6b2215971b8d899a96fad12a219d770d7cfd9b64
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-05-02 11:20:51 +0530

    feat: List in admin panel

--- admin/src/pages/list/List.jsx
+++ admin/src/pages/list/List.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
-import './List.css'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import './List.css';
 
 const List = () => {
+
+    const url = "http://localhost:4000"
+    const [list,setList] = useState([]);
+
+    const fetchList = async () => {
+        const response = await axios.get(`${url}/api/food/list`);
+        if (response.data.success) {
+            setList(response.data.data);
+        } else {
+            toast.error("An error occurred while fetching data. Please try again later!")
+        }
+    }
+
+    const removeFood = async(foodId) => {
+        const response = await axios.post(`${url}/api/food/remove`,{id:foodId})
+        await fetchList();
+        if (response.data.success) {
+            toast.success(response.data.message)
+        } else {
+            toast.error("Database connection error. Unable to remove the food item.")
+        }
+    }
+
+    useEffect(()=>{
+        fetchList();
+    },[])
+
     return (
-        <div>List</div>
+        <div className='list add flex-col'>
+            <p>All Food List</p>
+            <div className="list-table">
+                <div className="list-table-format title">
+                    <b>Image</b>
+                    <b>Name</b>
+                    <b>Category</b>
+                    <b>Price</b>
+                    <b>Action</b>
+                </div>
+                {list.map((item,index)=>{
+                    return (
+                        <div key={index} className='list-table-format'>
+                            <img className='img' src={`${url}/images/`+item.image} alt="" />
+                            <p>{item.name}</p>
+                            <p>{item.category}</p>
+                            <p>{item.price} LKR</p>
+                            <p onClick={()=>removeFood(item._id)} className='cursor'>x</p>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
     )
 }
 
