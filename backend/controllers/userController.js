f7e2ac1afe843f4f4ab036177b394b1f3e73f812
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-05-02 12:25:12 +0530

    feat: User controllers and routes

---
+++ backend/controllers/userController.js
@@ -0,0 +1,51 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import validator from 'validator';
+import userModel from "../models/userModel.js";
+
+//login
+const loginUser = async (req,res) =>{
+
+}
+
+const createToken = (id) => {
+    return jwt.sign({id},process.env.JWT_SECRET)
+}
+
+//register
+const registerUser = async (req,res) => {
+    const {name,password,email} = req.body;
+    try {
+        const exists = await userModel.findOne({email});
+        if (exists) {
+            return res.json({success:false,message:"User already exists!"})
+        }
+
+        if (!validator.isEmail(email)) {
+            return res.json({success:false,message:"Please enter a valid email!"})
+        }
+
+        if (password.length<8) {
+            return res.json({success:false,message:"Please enter a strong password!"})
+        }
+
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password,salt);
+
+        const newUser = new userModel({
+            name:name,
+            email:email,
+            password:hashedPassword
+        })
+
+        const user = await newUser.save()
+        const token = createToken(user._id)
+
+
+    } catch (error) {
+        
+    }
+}
+
+
+export { loginUser, registerUser };
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import cors from "cors"
+import 'dotenv/config'
 import express from "express"
 import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
+import userRouter from "./routes/userRoute.js"
 
 //app config
 const app = express()
@@ -17,6 +19,7 @@ connectDB();
 //api endpoints
 app.use("/api/food",foodRouter)
 app.use("/images",express.static('uploads'))
+app.use("api/user",userRouter)
 
 app.get("/",(req,res)=>{
     res.send("API Working")
