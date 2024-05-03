b2a3dfd1f0089655e4b340b0beb56dd64977c499
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-05-03 22:46:46 +0530

    feat: Remove from cart func

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -3,7 +3,7 @@ import userModel from "../models/userModel.js";
 //add to cart
 const addToCart = async (req,res) => {
     try {
-        let userData = await userModel.findOne({_id:req.body.userId})
+        let userData = await userModel.findById(req.body.userId);
         let cartData = await userData.cartData;
         if (!cartData[req.body.itemId]) {
             cartData[req.body.itemId] = 1;
@@ -20,7 +20,18 @@ const addToCart = async (req,res) => {
 
 //remove items
 const removeFromCart = async (req,res) => {
-
+    try {
+        let userData = await userModel.findById(req.body.userId);
+        let cartData = await userData.cartData;
+        if (cartData[req.body.itemId]>0) {
+            cartData[req.body.itemId] -= 1;
+        }
+        await userModel.findByIdAndUpdate(req.body.userId,{cartData});
+        res.json({success:true,message:"Successfully removed from the cart!"})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error removing from the cart!"})
+    }
 }
 
 //fetch user cart data
