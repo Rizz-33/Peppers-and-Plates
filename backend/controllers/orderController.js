5e82602fe3be702b4c7632d85d281235970d111b
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-05-04 20:05:45 +0530

    refactor: Order controller

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -48,7 +48,8 @@ const placeOrder = async (req,res) => {
 
         res.json({success:true,session_url:session.url})
     } catch (error) {
-        
+        console.log(error);
+        res.json({success:false,message:"Error in Process!"})
     }
 }
 
