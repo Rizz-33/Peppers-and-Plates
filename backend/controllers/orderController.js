6d6279cf6bc9613a1f939c47ba79112cb896202d
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-05-06 15:47:58 +0530

    feat: Fetch orders of each users

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -69,5 +69,15 @@ const verifyOrder = async (req,res) => {
     }
 }
 
-export { placeOrder, verifyOrder };
+const userOrders = async (req,res) => {
+    try {
+        const orders = await orderModel.find({userId:req.body.userId});
+        res.json({success:true,data:orders})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Uncaught Error. Please try again!"})
+    }
+}
+
+export { placeOrder, userOrders, verifyOrder };
 
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { placeOrder, verifyOrder } from "../controllers/orderController.js";
+import { placeOrder, userOrders, verifyOrder } from "../controllers/orderController.js";
 import authMiddleware from "../middleware/auth.js";
 
 const orderRouter = express.Router();
 
 orderRouter.post("/place",authMiddleware,placeOrder);
 orderRouter.post("/verify",verifyOrder);
+orderRouter.post("/userOrders",authMiddleware,userOrders)
 
 export default orderRouter;
